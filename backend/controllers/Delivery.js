6df92a01f182a20459fae9cbf857a46ba820f040
danang arif <danangarif202@gmail.com>
2022-03-28 18:29:19 +0700

    add function (create,update,delete) delivery

--- backend/controllers/Delivery.js
+++ backend/controllers/Delivery.js
@@ -34,3 +34,49 @@ export const getKurirById = async (req, res) => {
     res.json({ message: error.message });
   }
 };
+
+export const createKurir = async (req, res) => {
+  const payload = {
+    name: req.body.name
+  };
+  Delivery.create(payload)
+    .then((result) => {
+      res.status(200).json({
+        message: "Delivery Created",
+      });
+    })
+    .catch((error) => {
+      res.json({ message: error.message });
+    });
+};
+
+export const updateKurir = async (req, res) => {
+  const payload = {
+    name: req.body.name
+  };
+  Delivery.update(payload, {
+    where: {
+      id: req.params.id,
+    }
+  })
+    .then((result) => {
+      res.status(200).json({
+        message: "Delivery Updated",
+      });
+    })
+    .catch((error) => {
+      res.json({ message: error.message });
+    });
+};
+
+export const deleteKurir = async (req, res) => {
+  Delivery.destroy({ where: { id: req.params.id } })
+    .then((result) => {
+      res.status(200).json({
+        message: "Delivery Deleted",
+      });
+    })
+    .catch((error) => {
+      res.json({ message: error.message });
+    });
+};
