e59b5b8de768fed1c37e28781b153e87de58e96d
danang arif <danangarif202@gmail.com>
2022-03-28 18:22:58 +0700

    add function create,update,delete category

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -2,6 +2,9 @@ import express from "express";
 import {
   getAllCategory,
   getCategoryById,
+  createCategory,
+  updateCategory,
+  deleteCategory
 } from "../controllers/Category.js";
 import multer from "multer";
 const upload = multer({ dest: "uploads/" });
@@ -10,4 +13,7 @@ const router = express.Router();
 
 router.get("/", getAllCategory);
 router.get("/:id", getCategoryById);
+router.post("/", createCategory);
+router.patch("/:id", updateCategory);
+router.delete("/:id", deleteCategory);
 export default router;
