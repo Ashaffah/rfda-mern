a3dcd1be64212c1fc65e96a41d5b6bf458f92d9c
danang arif <danangarif202@gmail.com>
2022-03-30 06:20:09 +0700

    adjusment add product

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -1,34 +1,51 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom"; // Use useNavigate instead of useHistory sudah tidak digunakan di react v6
+import { Editor } from '@tinymce/tinymce-react';
 
 const AddProduct = () => {
-  const [title, setTitle] = useState("");
-  const [price, setPrice] = useState("");
-  const [image, setImage] = useState("");
+  const [dataProduct, setProduct] = useState({
+    title: "",
+    code: "",
+    price: 0,
+    selling_price: 0,
+    image: null,
+    category_id: null,
+    delivery_id: null,
+  });
   const history = useNavigate();
 
   const uploadImage = (e) => {
     const image = e.target.files[0];
-    console.log("path", image);
-    setImage(image);
+    setProduct((prevState) => ({
+      ...prevState,
+      image: image,
+    }));
   };
+
   const saveProduct = async (e) => {
     e.preventDefault();
 
     const data = new FormData();
-    data.append("title", title);
-    data.append("price", price);
-    data.append("image", image);
+    data.append("title", dataProduct.title);
+    data.append("code", dataProduct.code);
+    data.append("price", dataProduct.price);
+    data.append("selling_price", dataProduct.selling_price);
+    data.append("description", dataProduct.description);
+    data.append("category", dataProduct.category);
+    data.append("delivery", dataProduct.delivery);
+    data.append("image", dataProduct.image);
+
     await axios.post("http://localhost:5000/products", data, {
       headers: {
         "content-type": "multipart/form-data",
       },
     });
-    history("/"); // history.push("/"); no longer use push
+    history("/manage/product");
   };
   return (
     <div>
+      {console.log("data", dataProduct)}
       <form onSubmit={saveProduct}>
         <div className="field">
           <label className="label">Title</label>
@@ -36,8 +53,10 @@ const AddProduct = () => {
             className="input"
             type="text"
             placeholder="Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            onChange={(e) => setProduct((prevState) => ({
+              ...prevState,
+              title: e.target.value,
+            }))}
           />
         </div>
 
@@ -47,24 +66,70 @@ const AddProduct = () => {
             className="input"
             type="text"
             placeholder="Price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
+            onChange={(e) => setProduct((prevState) => ({
+              ...prevState,
+              price: e.target.value,
+            }))}
           />
         </div>
 
         <div className="field">
-          <label className="label">Image</label>
+          <label className="label">Selling Price</label>
           <input
             className="input"
-            type="file"
-            onChange={(e) => uploadImage(e)}
+            type="text"
+            placeholder="Price"
+            onChange={(e) => setProduct((prevState) => ({
+              ...prevState,
+              selling_price: e.target.value,
+            }))}
           />
+        </div>
+
+        {/* <Editor
+          onInit={(evt, editor) => editorRef.current = editor}
+          initialValue="<p>This is the initial content of the editor.</p>"
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist autolink lists link image charmap print preview anchor',
+              'searchreplace visualblocks code fullscreen',
+              'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar: 'undo redo | formatselect | ' +
+              'bold italic backcolor | alignleft aligncenter ' +
+              'alignright alignjustify | bullist numlist outdent indent | ' +
+              'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+          }}
+        /> */}
+
+        {console.log("dataProduct", dataProduct.image)}
+
+        <div class="file has-name is-boxed">
+          <label class="file-label">
+            <input class="file-input" type="file" name="resume" onChange={(e) => uploadImage(e)} />
+            <span class="file-cta">
+              <span class="file-icon">
+                <i class="fas fa-upload"></i>
+              </span>
+              <span class="file-label">
+                Choose a file…
+              </span>
+            </span>
+            {
+              dataProduct.image?.name &&
+              <span class="file-name" style={{ textAlign: "center" }}>
+                {dataProduct.image?.name}
+              </span>
+            }
+          </label>
         </div>
 
         <div className="field"></div>
         <button className="button is-primary">Save</button>
       </form>
-      {/* {title} - {price}  */}
     </div>
   );
 };
